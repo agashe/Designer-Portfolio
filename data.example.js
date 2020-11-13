a21c71dd025469c966989b620b0efce0836298f8
Mohamed Yousef <modi401@hotmail.com>
2020-11-13 14:48:51 +0200

    changes in data and start on layout

---
+++ data.example.js
@@ -0,0 +1,26 @@
+var data = {
+    "designer_name": "Example",
+    "work": [
+        {
+            "name": "project #1",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ",
+            "cover": "",
+            "images": [
+                ""
+            ]
+        }
+    ],
+    "personal": [
+        "image #1"
+    ],
+    "about": {
+        "title": "About",
+        "bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ",
+        "major": "test",
+        "skills": [
+            "skill1",
+            "skill2",
+            "skill3"
+        ]
+    }
+};
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*
+/**
  * Designer Portfolio
  *
  * Auther: Mohamed Yousef
@@ -6,3 +6,20 @@
  * GitHub: 
  */
 
+/**
+ * Befor start make sure to run command :
+ * cp data.example.js data.js
+ */
+
+/**
+ * Set page title
+ */
+
+document.title = data.designer_name + ' Portfolio';
+
+/**
+ * Set logo
+ */
+
+document.getElementById('logo').innerText = data.designer_name;
+document.getElementById('copyrights').innerHTML = data.designer_name + ' &copy; 2020';
